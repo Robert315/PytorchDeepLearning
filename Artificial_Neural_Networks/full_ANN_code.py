800c756ffec931077202e5a3a7616bc6b782cad3
Robert315 <nicolau.robert315@yahoo.ro>
2022-06-24 18:20:14 +0300

    Update

--- Artificial_Neural_Networks/full_ANN_code.py
+++ Artificial_Neural_Networks/full_ANN_code.py
@@ -25,7 +25,37 @@ def haversine_distance(df, lat1, long1, lat2, long2):
     d = (r * c)  # in kilometers
 
     return d
+
 df['dist_km'] = haversine_distance(df, 'pickup_latitude', 'pickup_longitude', 'dropoff_latitude', 'dropoff_longitude')
 df['pickup_datetime'] = pd.to_datetime(df['pickup_datetime'])  # convert in a datetime object
 
-print(df.info())
+my_time = df['pickup_datetime'][0]
+df['EDTdate'] = df['pickup_datetime'] - pd.Timedelta(hours=4)
+df['Hour'] = df['EDTdate'].dt.hour
+df['AMorPM'] = np.where(df['Hour'] < 12, 'AM', 'PM')
+df['Weekday'] = df['EDTdate'].dt.strftime('%a')
+
+cat_cols = ['Hour', 'AMorPM', "Weekday"]
+cont_cols = ['pickup_longitude','pickup_latitude', 'dropoff_longitude', 'dropoff_latitude', 'passenger_count', 'dist_km']
+y_col = ['fare_amount']
+
+# Convert our three categorical columns to category dtypes.
+for cat in cat_cols:
+    df[cat] = df[cat].astype('category')
+
+# Transform in numpy array
+hr = df['Hour'].cat.codes.values
+ampm = df['AMorPM'].cat.codes.values  # 0 or 1 for am or pm
+wkdy = df['Weekday'].cat.codes.values  # 0,...,6
+
+cats = np.stack([hr, ampm, wkdy], axis=1)
+
+cats = torch.tensor(cats, dtype=torch.int64)
+conts = np.stack([df[col].values for col in cont_cols], axis=1)
+conts = torch.tensor(conts, dtype=torch.float)
+
+y = torch.tensor(df[y_col].values, dtype=torch.float)
+cat_sizes = [len(df[col].cat.categories) for col in cat_cols]
+print(cat_sizes)
+emb_sizes = [(size,min(50, (size+1) // 2)) for size in cat_sizes]
+print(emb_sizes)

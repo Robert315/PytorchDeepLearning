7c53ba14738b12d8af0edef369929f35f40edb3a
Robert315 <nicolau.robert315@yahoo.ro>
2022-07-14 15:44:40 +0300

    Update

--- Convolution_Neural_Networks/MNIST_with_CNN.py
+++ Convolution_Neural_Networks/MNIST_with_CNN.py
@@ -9,6 +9,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
+import time
 
 
 transform = transforms.ToTensor()
@@ -17,7 +18,7 @@ test_data = datasets.MNIST(root='../Data', train=False, download=True, transform
 # print(train_data, tes  t_data)
 
 train_loader = DataLoader(train_data, batch_size=10, shuffle=True)
-test_loader = DataLoader(test_data, batch_size=10,shuffle=False)
+test_loader = DataLoader(test_data, batch_size=10, shuffle=False)
 
 # Define layers
 conv1 = nn.Conv2d(1, 6, 3, 1)  # 1 Color channel, 6 Filters(output channels, 3by3 Kernel, Stride=1
@@ -38,5 +39,84 @@ x = F.max_pool2d(x, 2, 2)
 x = F.relu(conv2(x))
 # Run the second pooling layer
 x = F.max_pool2d(x, 2, 2)  # 2by2 kernel, stride=2
-x = x.view(-1, 16 * 5 * 5)
+x = x.view(-1, 16*5*5)
 print(x.shape)
+
+
+class ConvolutionalNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 6, 3, 1)
+        self.conv2 = nn.Conv2d(6, 16, 3, 1)
+        self.fc1 = nn.Linear(5*5*16, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forword(self, X):
+        X = F.relu(self.conv1(X))
+        X = F.max_pool2d(X, 2, 2)
+        X = F.relu(self.conv2(X))
+        X = F.max_pool2d(X, 2, 2)
+        X = X.view(-1, 5*5*16)
+        X = F.relu(self.fc1(X))
+        X = F.relu(self.fc2(X))
+        X = self.fc3(X)
+        return F.log_softmax(X, dim=1)
+
+
+torch.manual_seed(42)
+model = ConvolutionalNetwork()
+print(model)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+
+start_time = time.time()
+
+epochs = 5
+train_losses = []
+test_losses = []
+train_correct = []
+test_correct = []
+
+for i in range(epochs):
+    trn_corr = 0
+    tst_corr = 0
+
+    # Run the training batches
+    for b, (X_train, y_train) in enumerate(train_loader):
+        b += 1
+
+        # Apply the model
+        y_pred = model(X_train)  # we don't flatten X-train here
+        loss = criterion(y_pred, y_train)
+
+        # Tally the number of correct predictions
+        predicted = torch.max(y_pred.data, 1)[1]
+        batch_corr = (predicted == y_train).sum()
+        trn_corr += batch_corr
+
+        # Update parameters
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    train_losses.append(loss)
+    train_correct.append(trn_corr)
+
+    # Run the testing batches
+    with torch.no_grad():
+        for b, (X_test, y_test) in enumerate(test_loader):
+            # Apply the model
+            y_val = model(X_test)
+
+            # Tally the number of correct predictions
+            predicted = torch.max(y_val.data, 1)[1]
+            tst_corr += (predicted == y_test).sum()
+
+    loss = criterion(y_val, y_test)
+    test_losses.append(loss)
+    test_correct.append(tst_corr)
+
+print(f'\nDuration: {time.time() - start_time:.0f} seconds')

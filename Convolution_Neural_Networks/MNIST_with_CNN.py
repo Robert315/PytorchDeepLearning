e4d593677062843b53eda883912b10e7873514fa
Robert315 <nicolau.robert315@yahoo.ro>
2022-07-18 15:48:41 +0300

    Update

--- Convolution_Neural_Networks/MNIST_with_CNN.py
+++ Convolution_Neural_Networks/MNIST_with_CNN.py
@@ -11,7 +11,6 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 import time
 
-
 transform = transforms.ToTensor()
 train_data = datasets.MNIST(root='../Data', train=True, download=True, transform=transform)
 test_data = datasets.MNIST(root='../Data', train=False, download=True, transform=transform)
@@ -39,7 +38,7 @@ x = F.max_pool2d(x, 2, 2)
 x = F.relu(conv2(x))
 # Run the second pooling layer
 x = F.max_pool2d(x, 2, 2)  # 2by2 kernel, stride=2
-x = x.view(-1, 16*5*5)
+x = x.view(-1, 16 * 5 * 5)
 print(x.shape)
 
 
@@ -50,9 +49,9 @@ class ConvolutionalNetwork(nn.Module):
         self.conv2 = nn.Conv2d(6, 16, 3, 1)
         self.fc1 = nn.Linear(5*5*16, 120)
         self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.fc3 = nn.Linear(84,10)
 
-    def forword(self, X):
+    def forward(self, X):
         X = F.relu(self.conv1(X))
         X = F.max_pool2d(X, 2, 2)
         X = F.relu(self.conv2(X))
@@ -63,7 +62,6 @@ class ConvolutionalNetwork(nn.Module):
         X = self.fc3(X)
         return F.log_softmax(X, dim=1)
 
-
 torch.manual_seed(42)
 model = ConvolutionalNetwork()
 print(model)
@@ -71,6 +69,7 @@ print(model)
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
+import time
 
 start_time = time.time()
 
@@ -102,6 +101,11 @@ for i in range(epochs):
         loss.backward()
         optimizer.step()
 
+        # Print interim results
+        if b % 600 == 0:
+            print(f'epoch: {i:2}  batch: {b:4} [{10 * b:6}/60000]  loss: {loss.item():10.8f}  \
+accuracy: {trn_corr.item() * 100 / (10 * b):7.3f}%')
+
     train_losses.append(loss)
     train_correct.append(trn_corr)
 
@@ -119,4 +123,34 @@ for i in range(epochs):
     test_losses.append(loss)
     test_correct.append(tst_corr)
 
-print(f'\nDuration: {time.time() - start_time:.0f} seconds')
+print(f'\nDuration: {time.time() - start_time:.0f} seconds')  # print the time elapsed
+
+# Extract the data all at once, not in batches
+test_load_all = DataLoader(test_data, batch_size=10000, shuffle=False)
+with torch.no_grad():
+    correct = 0
+    for X_test, y_test in test_load_all:
+        y_val = model(X_test)  # we don't flatten the data this time
+        predicted = torch.max(y_val,1)[1]
+        correct += (predicted == y_test).sum()
+print(f'Test accuracy: {correct.item()}/{len(test_data)} = {correct.item()*100/(len(test_data)):7.3f}%')
+
+#  We can track the index positions of "missed" predictions, and extract the corresponding image and label.
+#  We'll do this in batches to save screen space.
+misses = np.array([])
+for i in range(len(predicted.view(-1))):
+    if predicted[i] != y_test[i]:
+        misses = np.append(misses, i).astype('int64')
+
+# Display the number of misses
+len(misses)
+
+x = 2019
+plt.figure(figsize=(1, 1))
+plt.imshow(test_data[x][0].reshape((28, 28)), cmap="gist_yarg")
+plt.show()
+
+model.eval()
+with torch.no_grad():
+    new_pred = model(test_data[x][0].view(1, 1, 28, 28)).argmax()
+print("Predicted value:", new_pred.item())

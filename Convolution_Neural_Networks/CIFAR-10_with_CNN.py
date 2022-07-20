a6554cd6d1c87f625aedc2c236ccaaa7a1388db3
Robert315 <nicolau.robert315@yahoo.ro>
2022-07-20 12:00:27 +0300

    Update

--- Convolution_Neural_Networks/CIFAR-10_with_CNN.py
+++ Convolution_Neural_Networks/CIFAR-10_with_CNN.py
@@ -9,6 +9,8 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
+import time
+
 
 transform = transforms.ToTensor()
 
@@ -19,7 +21,7 @@ test_data = datasets.CIFAR10(root='../Data', train=False, download=True, transfo
 torch.manual_seed(101)  # for reproducible results
 
 train_loader = DataLoader(train_data, batch_size=10, shuffle=True)
-test_data = DataLoader(test_data, batch_size=10, shuffle=False)
+test_loader = DataLoader(test_data, batch_size=10, shuffle=False)
 
 # Define strings for labels
 class_name = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
@@ -49,3 +51,84 @@ class ConvolutionalNetwork(nn.Module):
         X = self.fc3(X)
         return F.log_softmax(X, dim=1)
 
+
+torch.manual_seed(101)
+model = ConvolutionalNetwork()
+# print(model)
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+# Train the model
+start_time = time.time()
+
+epochs = 10
+train_losses = []
+test_losses = []
+train_correct = []
+test_correct = []
+
+for i in range(epochs):
+    trn_corr = 0
+    tst_corr = 0
+
+    # Run the training batches
+    for b, (X_train, y_train) in enumerate(train_loader):
+        b += 1
+
+        # Apply the model
+        y_pred = model(X_train)
+        loss = criterion(y_pred, y_train)
+
+        # Tally the number of correct predictions
+        predicted = torch.max(y_pred.data, 1)[1]
+        batch_corr = (predicted == y_train).sum()
+        trn_corr += batch_corr
+
+        # Update parameters
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # Print interim results
+        if b % 1000 == 0:
+            print(f'epoch: {i:2}  batch: {b:4} [{10 * b:6}/50000]  loss: {loss.item():10.8f}  \
+accuracy: {trn_corr.item() * 100 / (10 * b):7.3f}%')
+
+    train_losses.append(loss)
+    train_correct.append(trn_corr)
+
+    # Run the testing batches
+    with torch.no_grad():
+        for b, (X_test, y_test) in enumerate(test_loader):
+            # Apply the model
+            y_val = model(X_test)
+
+            # Tally the number of correct predictions
+            predicted = torch.max(y_val.data, 1)[1]
+            tst_corr += (predicted == y_test).sum()
+
+    loss = criterion(y_val, y_test)
+    test_losses.append(loss)
+    test_correct.append(tst_corr)
+
+print(f'\nDuration: {time.time() - start_time:.0f} seconds')  # print the time elapsed
+
+torch.save(model.state_dict(), 'CIFAR-10_with_CNN.pt')
+
+# Display the confusion matrix
+test_load_all = DataLoader(test_data, batch_size=10000, shuffle=False)
+
+with torch.no_grad():
+    correct = 0
+    for X_test, y_test in test_load_all:
+        y_val = model(X_test)
+        predicted = torch.max(y_val,1)[1]
+        correct += (predicted == y_test).sum()
+
+arr = confusion_matrix(y_test.view(-1), predicted.view(-1))
+df_cm = pd.DataFrame(arr, class_names, class_names)
+plt.figure(figsize = (9, 6))
+sn.heatmap(df_cm, annot=True, fmt="d", cmap='BuGn')
+plt.xlabel("prediction")
+plt.ylabel("label (ground truth)")
+plt.show()

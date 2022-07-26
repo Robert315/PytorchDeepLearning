383f179c81095121af23f20b6fb2254b405daa60
Robert315 <nicolau.robert315@yahoo.ro>
2022-07-26 18:05:46 +0300

    Update

---
+++ Convolution_Neural_Networks/CNN_on_custom_imagine.py
@@ -0,0 +1,92 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms, models  # add models to the list
+from torchvision.utils import make_grid
+import os
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings("ignore")
+
+
+train_transform = transforms.Compose([
+    transforms.RandomRotation(10),
+    transforms.RandomHorizontalFlip(),
+    transforms.Resize(224),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406],
+                         [0.229, 0.224, 0.225])
+])
+
+test_transforms = transforms.Compose([
+    transforms.Resize(224),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406],
+                         [0.229, 0.224, 0.225])
+])
+
+root = '../Data/CATS_DOGS/'
+
+train_data = datasets.ImageFolder(os.path.join(root, 'train'), transform=train_transform)
+test_data = datasets.ImageFolder(os.path.join(root, 'test'), transform=test_transforms)
+
+torch.manual_seed(42)
+train_loader = DataLoader(train_data, batch_size=10, shuffle=True)
+test_loader = DataLoader(test_data, batch_size=10, shuffle=True)
+
+class_name = train_data.classes
+# print(class_name)
+# print(len(train_data), ',', len(test_data))
+
+for images, labels in train_loader:
+    break
+
+im = make_grid(images, nrow=5)
+
+# Inverse normalize the images
+inv_normalize = transforms.Normalize(
+    mean=[-0.485/0.229, -0.456/0.224, -0.406/0.225],
+    std=[1/0.229, 1/0.224, 1/0.225]
+)
+im_inv = inv_normalize(im)
+
+# Print the images
+plt.figure(figsize=(12,4))
+plt.imshow(np.transpose(im_inv.numpy(), (1, 2, 0)))
+# plt.show()
+
+class ConvolutionalNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 3, 1)
+        self.conv2 = nn.Conv2d(6, 16, 3, 1)
+        self.fc1 = nn.Linear(54*54*16, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 2)
+
+    def forward(self, X):
+        X = F.relu(self.conv1(X))
+        X = F.max_pool2d(X, 2, 2)
+        X = F.relu(self.conv2(X))
+        X = F.max_pool2d(X, 2, 2)
+        X = X.view(-1, 54*54*16)
+        X = F.relu(self.fc1(X))
+        X = F.relu(self.fc2(X))
+        X = self.fc3(X)
+        return F.log_softmax(X, dim=1)
+
+torch.manual_seed(42)
+model = ConvolutionalNetwork()
+print(model)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+for param in model.parameters():
+    print(param.numel())
--- Convolution_Neural_Networks/loading_real_image_data.py
+++ Convolution_Neural_Networks/loading_real_image_data.py
@@ -52,4 +52,58 @@ transform = transforms.Compose([
 ])
 im = transform(dog)
 print(im.shape)
-plt.imshow(np.transpose(im.numpy(), (1, 2, 3)))
+plt.imshow(np.transpose(im.numpy(), (1, 2, 0)))
+# plt.show()
+# print(im)
+
+
+transform = transforms.Compose([
+    transforms.Resize(224),
+    transforms.ToTensor()
+])
+im = transform(dog)
+print(im.shape)
+plt.imshow(np.transpose(im.numpy(), (1, 2, 0)))
+# plt.show()
+# print(im)
+
+transform = transforms.Compose([
+    transforms.Resize(224),
+    transforms.CenterCrop(224),
+    transforms.ToTensor()
+])
+im = transform(dog)
+print(im.shape)
+plt.imshow(np.transpose(im.numpy(), (1, 2, 0)))
+# plt.show()
+# print(im)
+
+transform = transforms.Compose([
+    transforms.RandomHorizontalFlip(p=1),
+    transforms.ToTensor()
+])
+im = transform(dog)
+print(im.shape)
+plt.imshow(np.transpose(im.numpy(), (1, 2, 0)))
+# plt.show()
+# print(im)
+
+transform = transforms.Compose([
+    transforms.RandomRotation(30),
+    transforms.ToTensor()
+])
+im = transform(dog)
+print(im.shape)
+plt.imshow(np.transpose(im.numpy(), (1, 2, 0)))
+# plt.show()
+# print(im)
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+im = transform(dog)
+print(im.shape)
+plt.imshow(np.transpose(im.numpy(), (1, 2, 0)))
+plt.show()
+print(im)
